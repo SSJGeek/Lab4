ee84c1781af850161a3683641b09879acfb65a4a
Aarya <Aarya@LAPTOP-UCG16NUF>
2017-09-26 20:18:37 -0500

    This is the cleaned up file in accordance with checkstyle's conventions.

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,44 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * A class that does something.
+ *
+ */
+public class ReclamationProject {
+
+/**
+* A class that does something.
+* @param word1 is a String
+* @param word2 is a String
+* @return a String that contains every letter common to both words starting from the beginning.
+*         ex. "Aardvark" and "Avocado" returns "A"
+*             "Aardvark" and "Aardvark" returns "Aarvar" [last character is omitted]
+*/
+public static String dictionaryCompare(final String  word1, final String  word2) {
+String delta = word1; //just a temporary reference
+String gamma = word2; //just a temporary reference
+
+//this is used to swap the order of the inputs based on
+
+    if (delta.length() > gamma.length()) {
+            String c = delta;
+            delta = gamma;
+            gamma = c;
+            }
+        String result = "";
+
+        for (int posInDelta = 0; posInDelta < delta.length(); posInDelta++) {
+            for (int oppositeSideOfDelta = delta.length() - posInDelta;
+                    oppositeSideOfDelta > 0; oppositeSideOfDelta--) {
+                for (int posInGamma = 0; posInGamma < gamma.length() - oppositeSideOfDelta;
+                        posInGamma++) {
+                    if ((delta.regionMatches(posInDelta, gamma, posInGamma, oppositeSideOfDelta)
+                            &&
+                         oppositeSideOfDelta > result.length())) {
+                        result = delta.substring(posInDelta, posInDelta + oppositeSideOfDelta);
+                    }
+                        }
+                }
+        } return result; }
 }
